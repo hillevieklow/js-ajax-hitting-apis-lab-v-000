e435e90554cc0961acedc00726deebfc03491dfd
Hillevi Eklöw <hillevieklow@gmail.com>
2018-12-30 10:30:47 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // your code here
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/hillevieklow/repos');
+  const username = document.getElementById('username').value;
+  req.addEventListener("load", displayRepositories);
+  req.open("GET",
+  'https://api.github.com/users/' + username + '/repos');
   req.send();
+  return false;
 }
 
 function showRepositories() {
